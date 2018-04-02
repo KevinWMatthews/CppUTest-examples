e92b6c35549481b7f8f54de08af9ce9a66b0b132
Kevin W Matthews <KevinWMatthews@gmail.com>
2018-04-01 19:07:25 -0700

    Implement comparator

--- tests/MockComparator/RunAllTests.cpp
+++ tests/MockComparator/RunAllTests.cpp
@@ -13,12 +13,28 @@ class SomeStructComparator : public MockNamedValueComparator
 public:
     virtual bool isEqual(const void* object1, const void* object2)
     {
+        const SOME_STRUCT * some_struct1 = (SOME_STRUCT *)object1;
+        const SOME_STRUCT * some_struct2 = (SOME_STRUCT *)object2;
+
+        if (some_struct1->thing1 != some_struct2->thing1)
+            return false;
+
+        if (some_struct1->thing2 != some_struct2->thing2)
+            return false;
+
         return true;
     }
 
     virtual SimpleString valueToString(const void* object)
     {
-        return StringFrom(object);
+        const SOME_STRUCT * some_struct = (SOME_STRUCT *)object;
+
+        SimpleString output;
+
+        output += StringFrom(object);
+        output += StringFrom(" - ") + StringFrom("thing1: ") + StringFrom(some_struct->thing1);
+        output += StringFrom("; ") + StringFrom("thing2: ") + StringFrom(some_struct->thing2);
+        return output;
     }
 };
 
--- tests/MockComparator/Test_MockComparator.cpp
+++ tests/MockComparator/Test_MockComparator.cpp
@@ -24,9 +24,29 @@ TEST_GROUP(TestWithMockComparator)
 TEST(TestWithMockComparator, call_function_with_comparator)
 {
     SOME_STRUCT some_struct = {};
+    some_struct.thing1 = 41;
+    some_struct.thing2 = 42;
 
     mock("SomeMock").expectOneCall("SomeLibrary_DoesNothingWithStructParameter")
         .withParameterOfType("SOME_STRUCT", "param", &some_struct);
 
     SomeLibrary_DoesNothingWithStructParameter(&some_struct);
 }
+
+#if 0
+// This test is configured to fail
+TEST(TestWithMockComparator, comparator_can_fail)
+{
+    SOME_STRUCT some_struct = {};
+    some_struct.thing1 = 41;
+    some_struct.thing2 = 42;
+
+    mock("SomeMock").expectOneCall("SomeLibrary_DoesNothingWithStructParameter")
+        .withParameterOfType("SOME_STRUCT", "param", &some_struct);
+
+    SOME_STRUCT other_struct = {};
+    other_struct.thing1 = 66;
+    other_struct.thing2 = 67;
+    SomeLibrary_DoesNothingWithStructParameter(&other_struct);
+}
+#endif
