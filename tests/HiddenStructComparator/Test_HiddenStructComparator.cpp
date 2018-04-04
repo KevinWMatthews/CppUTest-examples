77dd4dd76c48aaeb9afa4fa825b8924e16e8636b
Kevin W Matthews <KevinWMatthews@gmail.com>
2018-04-04 08:46:20 -0700

    Use handles in comparator

--- tests/HiddenStructComparator/Test_HiddenStructComparator.cpp
+++ tests/HiddenStructComparator/Test_HiddenStructComparator.cpp
@@ -12,12 +12,16 @@ extern "C"
 TEST_GROUP(HiddenStructComparator)
 {
     HiddenStructComparator local_comparator;
+    HIDDEN_STRUCT_DEFINITION hidden_struct;
     HIDDEN_STRUCT_HANDLE hidden_struct_handle;
+    HIDDEN_STRUCT_DEFINITION hidden_struct2;
     HIDDEN_STRUCT_HANDLE hidden_struct_handle2;
     bool result;
 
     void setup()
     {
+        hidden_struct_handle = &hidden_struct;
+        hidden_struct_handle2 = &hidden_struct2;
     }
 
     void teardown()
@@ -27,12 +31,19 @@ TEST_GROUP(HiddenStructComparator)
 
 TEST(HiddenStructComparator, structs_can_be_equal)
 {
-    HIDDEN_STRUCT_DEFINITION hidden_struct = {};
-    HIDDEN_STRUCT_DEFINITION hidden_struct2 = {};
-
     hidden_struct.test_value = 12;
     hidden_struct2.test_value = 12;
 
     result = local_comparator.isEqual(&hidden_struct, &hidden_struct2);
+    CHECK_TRUE(result);
+}
+
+TEST(HiddenStructComparator, comparator_can_use_handles)
+{
+    // Better to initialize the struct's themselves?
+    hidden_struct_handle->test_value = 13;
+    hidden_struct_handle2->test_value = 13;
+
+    result = local_comparator.isEqual(hidden_struct_handle, hidden_struct_handle2);
     CHECK_TRUE(result);
 }
