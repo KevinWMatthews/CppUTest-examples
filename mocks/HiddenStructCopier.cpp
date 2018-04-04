2138b985bdbf642d9df2f37232eb141141309550
Kevin W Matthews <KevinWMatthews@gmail.com>
2018-04-04 09:06:44 -0700

    Add null checks to HiddenStructCopier

--- mocks/HiddenStructCopier.cpp
+++ mocks/HiddenStructCopier.cpp
@@ -10,5 +10,11 @@ void HiddenStructCopier::copy(void* out, const void* in)
     HIDDEN_STRUCT_DEFINITION * hidden_struct_out = (HIDDEN_STRUCT_DEFINITION *)out;
     const HIDDEN_STRUCT_DEFINITION * hidden_struct_in = (const HIDDEN_STRUCT_DEFINITION *)in;
 
+    if (hidden_struct_out == NULL)
+        return;
+    if (hidden_struct_in == NULL)
+        return;
+
+
     hidden_struct_out->test_value = hidden_struct_in->test_value;
 }
--- tests/HiddenStructCopier/Test_HiddenStructCopier.cpp
+++ tests/HiddenStructCopier/Test_HiddenStructCopier.cpp
@@ -32,3 +32,13 @@ TEST(HiddenStructCopier, can_copy_object)
     local_copier.copy(&hidden_struct2, &hidden_struct);
     LONGS_EQUAL( 42, hidden_struct2.test_value );
 }
+
+TEST(HiddenStructCopier, null_output_object_does_not_segfault)
+{
+    local_copier.copy(NULL, &hidden_struct2);
+}
+
+TEST(HiddenStructCopier, null_input_object_does_not_segfault)
+{
+    local_copier.copy(NULL, &hidden_struct2);
+}
