f4f7d80a9b8024a847b60353d9709f66e02ef82c
Kevin W Matthews <KevinWMatthews@gmail.com>
2018-04-05 08:32:54 -0700

    Add more documentation to SomeLibrary header file

--- include/SomeLibrary.h
+++ include/SomeLibrary.h
@@ -24,7 +24,10 @@ void SomeLibrary_DoesNothingWithStructParameter(SOME_STRUCT *self);
 // The library does some internal magic that external modules can't see.
 void SomeLibrary_FillStructParameter(SOME_STRUCT *self);
 
+// Manually fill the struct from the other parameters.
 void SomeLibrary_FillStructFromValues(int thing1, int thing2, SOME_STRUCT *self);
+
+// Use a mock copier to fill the struct from other parameters.
 void SomeLibrary_FillStructFromValuesWithCopier(int thing1, int thing2, SOME_STRUCT *self);
 
 typedef struct SOME_STRUCT_INIT_PARAMS
@@ -32,10 +35,18 @@ typedef struct SOME_STRUCT_INIT_PARAMS
     int init_value1;
     int init_value2;
 } SOME_STRUCT_INIT_PARAMS;
+// Manually fill the struct from the struct of initialization parameters.
 void SomeLibrary_FillStructFromStruct(SOME_STRUCT_INIT_PARAMS *params, SOME_STRUCT *self);
+
+// Use a mock copier fill the struct from the struct of initialization parameters.
 void SomeLibrary_FillStructFromStructWithCopier(SOME_STRUCT_INIT_PARAMS *params, SOME_STRUCT *self);
 
-
+/*
+ * Show that copiers and comparators can work when data hiding is used.
+ * The production code struct definition is not available to test code,
+ * so a mock definition must provided in test code.
+ * In this case there is a mock header file for the library.
+ */
 typedef struct HIDDEN_STRUCT_DEFINITION * HIDDEN_STRUCT_HANDLE;
 
 // void SomeLibrary_CompareHiddenStruct(HIDDEN_STRUCT_HANDLE handle);
